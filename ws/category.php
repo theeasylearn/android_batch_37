ddb344b588f383db5c16ad2b4ac3877d9efb392a
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2021-10-20 20:27:19 +0530

    connection and category

--- ws/category.php
+++ ws/category.php
@@ -3,17 +3,12 @@
     purpose : used to get all live categories 
     how to call : http://localhost:8080/android_batch_37/ws/category.php
     input: Nothing 
-    output: 
+    output: [{"error":"no"},{"total":6},{"id":"1","title":"laptop","photo":"laptop.jpg"},{"id":"2","title":"mobile","photo":"mobile.jpg"},{"id":"3","title":"book","photo":"books.jpg"},{"id":"4","title":"Cookies & waffers","photo":"Cookies.jpg"},{"id":"5","title":"Washing Powders","photo":"washing_powders.jpg"},{"id":"6","title":"shampoo","photo":"shampoo.jpg"}]
     */
-    $response = array();
-    //step -1 select database server 
-    $link = mysqli_connect("localhost:3306","root","");
-
-    //step -2 select database 
-    mysqli_select_db($link,"android_batch_34");
+    require_once("../inc/connection.php");
     $sql = "select id,title,photo from category";
     //step -3 run query 
-    $table = mysqli_query($link,$sql);
+    $table = mysqli_query($link,$sql) or ReturnError(null,__LINE__);
     array_push($response,array("total"=>mysqli_num_rows($table)));
     while($row = mysqli_fetch_assoc($table))
     {
